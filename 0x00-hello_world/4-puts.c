e8d2482b6552a463f9c71a31fbd65f784d3c6649
lorddiamon <diamondavira3@gmail.com>
2022-06-30 15:35:54 -0700

    Task number 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* Description: main - Prints out a string to standout.
-* Return: 0 if success.
+* main -entry point
+* Return: Always 0 (sucess)
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
