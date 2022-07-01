a65891b8e551e0d656cfda874f6b785f8f0f412e
lorddiamon <diamondavira3@gmail.com>
2022-07-01 09:57:53 -0700

    Task 6 at it again

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,10 +8,12 @@ int main(void)
 {
 int ch;
 
-for (ch = 97; ch <= 122; ch++)
+for (ch = 48; ch <= 57; ch++)
 {
+
 putchar(ch);
 }
+
 putchar(10); /* this is an ascii code for new line*/
 
 return (0);
