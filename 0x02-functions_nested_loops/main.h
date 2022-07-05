f2fd6b11c08ff9c47dd307c24331d334807c719a
lorddiamon <diamondavira3@gmail.com>
2022-07-05 07:07:40 -0700

    Hot fix for Task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,24 +1,13 @@
-int _putchar(char k);
-
+int _putchar(char c);
 void print_alphabet(void);
-
-void print_alphabet_x10(void)
-
+void print_alphabet_x10(void);
 int _islower(int c);
-
 int _isalpha(int c);
-
 int print_sign(int n);
-
-int _abs(int a);
-
-int print_last_digit(int a);
-
+int _abs(int);
+int print_last_digit(int);
 void jack_bauer(void);
-
 void times_table(void);
-
-int add(int a, int b);
-
+int add(int, int);
 void print_to_98(int n);
-"main.h" 23L, 297C
+void print_times_table(int n);
