51a4d1815419c9148953c4de2f4b7c0b2084274c
lorddiamon <diamondavira3@gmail.com>
2022-07-07 04:57:18 -0700

    task 11 hot fix

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,7 +10,7 @@ int main(void)
 {
 long int n;
 long int max;
-long int i
+long int i;
 n = 612852475143;
 max = -1;
 while (n % 2 == 0)
@@ -18,7 +18,7 @@ while (n % 2 == 0)
 max = 2;
 n /= 2
 }
-for (i = 3; i <= sqrt(n); i = i +2)
+for (i = 3; i <= sqrt(n); i = i + 2)
 {
 while (n % i == 0)
 {
@@ -26,4 +26,8 @@ max = i;
 n = n / i;
 }
 }
+if (n > 2)
+max = n;
+printf("%ld\n", max);
+return (0);
 }
