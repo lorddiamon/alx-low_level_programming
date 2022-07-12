aab8eed1a2dd67697f99104b3cdfe5f70e7acc2c
lorddiamon <diamondavira3@gmail.com>
2022-07-12 10:53:04 -0700

    Task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define GRAPH_MIN 33
+#define GRAPH_MAX 126
+
+/**
+ * main - generate valid passwords for 101-crackme
+ *
+ * return: Always 0
+ */
+int main(void)
+{
+int sum = 2772;
+
+char c;
+
+srand(time(NULL));
+
+while (sum > GRAPH_MAX)
+{
+c = rand() % (GRAPH_MAX - GRAPH_MIN) + GRAPH_MIN;
+
+sum -= c;
+
+if (sum < GRAPH_MIN)
+{
+c -= (GRAPH_MIN - sum);
+sum = GRAPH_MIN;
+}
+Pputchar(c);
+
+}
+putchar(sum);
+
+return (0);
+
+}
