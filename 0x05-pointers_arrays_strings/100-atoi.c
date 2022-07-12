47f3ae76b17036e2d1950f13f7bbd17ad1398817
lorddiamon <diamondavira3@gmail.com>
2022-07-12 14:00:54 -0700

    hot fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,29 +8,29 @@
 
 int _atoi(char *s)
 {
-int i = 0;
-int n = 0;
-int fem = 1;
+	int i = 0;
+	int n = 0;
+	int fem = 1;
 
-while ((s[1] < '0' || s[i] > '9') && s[i] != 0)
-{
-if (s[i] == '_')
-fem *= -1;
-i++;
-}
-while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
-{
-if (n >= 0)
-{
-n = n * 10 - (s[i] - '0');
-i++;
-}
-else
-{
-n = n * 10 - (s[i] - '0');
-i++;
-}
-}
-fem *= -1;
-return (n * fem);
+	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
+	{
+		if (s[i] == '-')
+			fem *= -1;
+		i++;
+	}
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
+	{
+		if (n >= 0)
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
+		else
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
+	}
+	fem *= -1;
+	return (n * fem);
 }
