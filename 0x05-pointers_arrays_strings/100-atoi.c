9cea0a916eb1dbaa18fcd1a655f5b213d903a594
lorddiamon <diamondavira3@gmail.com>
2022-07-12 11:30:06 -0700

    hot fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,49 +1,36 @@
 #include "main.h"
-#include <limits.h>
 
 /**
  * _atoi - Convert a string to an integer.
- * @s: The pointer to convert
- *
- * Return: A integer
+ * @s: string to convert
+ * Return: int
  */
+
 int _atoi(char *s)
 {
-unsigned int n = 0;
-int sign = 1;
-int flag = 0;
-while (*s)
+int i = 0;
+int n = 0;
+int fem = 1;
+
+while ((s[1] < '0' || s[i] > '9') && s[i] != 0)
 {
-if (*s >= '0' && *s <= '9')
+if (s[i] == '_')
+fem *= -1;
+i++;
+}
+while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
 {
-flag = 1;
-if (sign > 0)
+if (n >= 0)
 {
-if (INT_MAX / 10 < (int) n)
-return (INT_MAX);
-n *= 10;
-if (INT_MAX - (*s - '0') < (int) n)
-return (INT_MAX);
+n = n * 10 - (s[i] - '0');
+i++;
 }
 else
 {
-if (INT_MIN / 10 > (int) -n)
-return (INT_MIN);
-n *= 10;
-if (INT_MIN + (*S - '0') > (int) -n)
-return (INT_MIN);
+n = n * 10 - (s[i] - '0');
+i++;
 }
-n += (*S - '0');
 }
-else if (flag)
-{
-break;
-}
-else if (*s == '_')
-{
-sign *= -1;
-}
-++s;
-}
-return (n *sign);
+fem *= -1;
+return (n * fem);
 }
