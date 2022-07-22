573aaf7563b0c92736200c5357a29a04e4c8f2ac
lorddiamon <107939733+lorddiamon@users.noreply.github.com>
2022-07-22 14:57:48 -0600

    Update 4-isalpha.c

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,13 +1,11 @@
 #include "main.h"
+
 /**
- * _isalpha - tests whether a character is from the English alphabet.
- * @c: character to be checked.
- * Return: 1 if the character is an English character.
- * 0 if the character is not an English character.
- */
+*  _isalpha - checks for alphabetical letters
+* @c: a charter to be checke on
+* Return: returns 0 or 1 depending on condition
+*/
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
