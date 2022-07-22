1a70687e0ade5c88f1276f97b099e18d60300567
lorddiamon <107939733+lorddiamon@users.noreply.github.com>
2022-07-22 14:18:33 -0600

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,5 +1,20 @@
 #include "main.h"
-void _puts(char *s)
+
+/**
+ * _puts - function that prints a string
+ *
+ * @str: pointer to char
+ * Return: nothing
+ */
+
+void _puts(char *str)
 {
-		_putchar('0');
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 1;
+	}
+	_putchar('\n');
 }
