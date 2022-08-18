7b97190da9fb9637fd3c286a0ca008af49aeea15
lorddiamon <107939733+lorddiamon@users.noreply.github.com>
2022-08-18 14:32:18 -0600

    Create main.h

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,19 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
+#endif
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
